6db4a0bee946bead59c739beb6503c7a4484791d
otogan <onurtogan10@gmail.com>
2020-06-01 21:59:27 -0500

    readability-score complete stage 1

--- Readability-Score/Readability Score/task/src/readability/Main.java
+++ Readability-Score/Readability Score/task/src/readability/Main.java
@@ -1,7 +1,11 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input.length() > 100 ? "HARD" : "EASY");
     }
 }
