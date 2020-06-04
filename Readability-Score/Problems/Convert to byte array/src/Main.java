52f299cb9c0820b97269f7a83da96dff0db6a76d
otogan <onurtogan10@gmail.com>
2020-06-03 22:01:57 -0500

    code improvement

--- Readability-Score/Problems/Convert to byte array/src/Main.java
+++ Readability-Score/Problems/Convert to byte array/src/Main.java
@@ -6,13 +6,13 @@ class Converter {
     public static char[] convert(String[] words) throws IOException {
         // implement the method
         CharArrayWriter writer = new CharArrayWriter();
-        Arrays.stream(words).forEach(s -> {
+        for (String s : words) {
             try {
                 writer.write(s);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        });
+        }
         return writer.toCharArray();
     }
 }
